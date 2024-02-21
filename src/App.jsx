0afcb3baa4d5882312663a8288e3ea00f33ba678
ASGJxy <armasjack11@gmail.com>
2024-02-20 18:10:27 -0600

    add erase button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 function App() {
   const [input, setInput] = useState("");
+  const erase = () => {
+    setInput(input.slice(0, -1));
+  };
   return (
     <div>
       <span>{input}</span>
@@ -11,13 +14,23 @@ function App() {
       >
         +
       </button>
-      <button>1</button>
+      <button
+        onClick={() => {
+          setInput(input + "1");
+        }}
+      >
+        1
+      </button>
       <button>2</button>
       <button>3</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
       <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>0</button>
+      <button onClick={erase}>erase</button>
     </div>
   );
 }
